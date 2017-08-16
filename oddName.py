36456cb3148b03904760a6408ec12a3824594e4a
jc320748 <cain.mcdonald@my.jcu.edu.au>
2017-08-16 12:49:55 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,7 @@
 """ Cain McDonald"""
+name = input("Please enter your name:")
+if len(name) <= 0:
+    print("Invalid Name")
+    name = input("Please enter your name:")
+for char in name[::2]:
+    print("{}".format(char))
